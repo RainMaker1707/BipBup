c48348db9e9a1dd65e6ab4bb9ede8bc0c27ef8de
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-05-08 17:40:24 +0200

    little opti

--- scripts/mute.js
+++ scripts/mute.js
@@ -10,9 +10,9 @@ module.exports = {
             message.channel.send("Vous n'avez pas la permission d'effectuer cette action!").catch()
         }
         else {
-            if (message.mentions.members.first()) {
-                message.mentions.members.first().roles.add(
-                    message.guild.roles.cache.find(role => role.name === "muted")
+            let nameTag = message.mentions.members.first();
+            if (nameTag) {
+                nameTag.roles.add(message.guild.roles.cache.find(role => role.name === "muted")
                 ).catch((err) => {
                     console.log(err)
                 }).then(() => {
@@ -23,14 +23,12 @@ module.exports = {
                     }
                     if(Number.isInteger(timeOut)) message.channel.send("est maintenant mute pour une duré de "
                         +timeOut+" minutes").catch();
-                    else message.channel.send("<@!"+message.mentions.members.first()
-                        + "> est maintenant mute pour une duré de "
+                    else message.channel.send("<@!" + nameTag + "> est maintenant mute pour une duré de "
                         +Math.trunc(timeOut)+"min "+Math.round((timeOut%1)*60)+"sec").catch();
                     sleep(timeOut * 60000).then(() => {
-                        message.mentions.members.first().roles.remove(
-                            message.guild.roles.cache.find(role => role.name === "muted")
+                        nameTag.roles.remove(message.guild.roles.cache.find(role => role.name === "muted")
                         ).catch().then(() => {
-                            message.channel.send("<@!"+message.mentions.members.first() +
+                            message.channel.send("<@!"+ nameTag +
                                 "> Tu es maintenant unmute, essaie d'être plus calme a l'avenir").catch();
                         });
                     })
--- scripts/unmute.js
+++ scripts/unmute.js
@@ -2,13 +2,12 @@ let tools = require('./tools');
 
 module.exports = (message) => {
     if(tools.checkPerm(message, 'KICK_MEMBERS')){
-        if(message.mentions.members.first()){
-            if(message.mentions.members.first().roles.cache.some((role) => role.name === 'muted')){
-                message.mentions.members.first().roles.remove(
-                    message.guild.roles.cache.find(role => role.name === "muted")
+        let nameTag = message.mentions.members.first();
+        if(nameTag){
+            if(nameTag.roles.cache.some((role) => role.name === 'muted')){
+                nameTag.roles.remove( message.guild.roles.cache.find(role => role.name === "muted")
                 ).catch().then( ()=> {
-                    message.channel.send("<@!" + message.mentions.members.first() +
-                        "> est maintenant libéré de ses entraves").catch();
+                    message.channel.send("<@!" + nameTag + "> est maintenant libéré de ses entraves").catch();
                 });
             }else message.channel.send("Cette personne n'est pas mute...バカ").catch()
         }else message.channel.send("La commande est mal formatée: .unmute nameTag").catch()
