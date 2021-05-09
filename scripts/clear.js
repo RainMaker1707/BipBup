cc966620daf02bf76060d52819368ae2a7f1921b
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-05-09 23:07:41 +0200

    remove useless print (last patch)

--- scripts/clear.js
+++ scripts/clear.js
@@ -10,11 +10,9 @@ module.exports = (message)=>{
   if(tools.checkPerm(message, "KICK_MEMBERS")){
       let cnt =  message.content.slice(CFG.PREFIX.length).trim().split(' ');
       cnt.shift();
-      console.log(parseInt(cnt[0]) <= 100);
       if(!cnt[0]) {
           clear(25).catch();
       }else if (parseInt(cnt[0]) <= 100) {
-          console.log(cnt[0]);
           clear(parseInt(cnt[0])).catch();
       } else tools.answer(message, "Vous pouvez supprimez 100 messages maximum");
   }else tools.answer(message, "Vous n'avez pas la permission de supprimer des messages");
