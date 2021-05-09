2ff0f919e57e375d12bfea658410c62fd7b31b8a
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-05-09 22:06:17 +0200

    patch it

--- scripts/clear.js
+++ scripts/clear.js
@@ -10,10 +10,12 @@ module.exports = (message)=>{
   if(tools.checkPerm(message, "KICK_MEMBERS")){
       let cnt =  message.content.slice(CFG.PREFIX.length).trim().split(' ');
       cnt.shift();
+      console.log(parseInt(cnt[0]) <= 100);
       if(!cnt[0]) {
           clear(25).catch();
-      }else if (Number(cnt[0]) <= 100) {
-          clear(parseInt(cnt[0])).catch((e)=>tools.answer(message, 'error '+e));
+      }else if (parseInt(cnt[0]) <= 100) {
+          console.log(cnt[0]);
+          clear(parseInt(cnt[0])).catch();
       } else tools.answer(message, "Vous pouvez supprimez 100 messages maximum");
   }else tools.answer(message, "Vous n'avez pas la permission de supprimer des messages");
 };
